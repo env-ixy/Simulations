09aac4062a150a6a2db31744c0165a8cfae0bb3d
Meet Shah <meetshah9625@gmail.com>
2026-05-27 15:44:08 +0530

    Added air resistance

--- Projectile motion/main.py
+++ Projectile motion/main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random as r
 
 # CONSTANTS
-g = -9.8 # (-ve since going up)
-dt = 0.01  
+g = -9.8  # (-ve since going up)
+dt = 0.01
 theta = np.pi / 4  # 45deg in radians
-v0 = 20  
+v0 = 100
 
 # INITIAL TIME
 t = 0
@@ -18,17 +19,22 @@ y = 0
 vx = v0 * np.cos(theta)
 vy = v0 * np.sin(theta)
 
+# AIR RESISTANCE
+k = r.uniform(0, 0.2) # random because why not
+
 # ARRAYS FOR PLOTTING
 xs = []
 ys = []
 
 while y >= 0:
-    vy += g*dt # v_t' = v_t + gdt
-    x += vx*dt # change horizontal position
-    y += vy*dt # change veritical position 
+    vy += g * dt  # v_t' = v_t + gdt
+    vx -= k * vx * dt # air resistance sideways
+    vy -= k * vy * dt # air resistance downward
+    x += vx * dt # change horizontal position
+    y += vy * dt # change vertical position
     xs.append(x)
     ys.append(y)
-    t+=dt # update time
+    t += dt # update time
 
 plt.plot(xs, ys)
 plt.show()
