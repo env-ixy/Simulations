822f6d9d77cdb270491992d41f8dab343d96495f
Meet Shah <meetshah9625@gmail.com>
2026-05-30 17:10:43 +0530

    2D walk with 10,000

--- Random Walks/README.md
+++ Random Walks/README.md
@@ -23,6 +23,8 @@ $$
 
 where $a = 0 \text{ OR } a = 1$ because numpy random numbers are between 0 and 1, I round it to the nearest integer.
 
+# 1D results
+
 After 10 runs, the best position, the last position and the mean position are as follows:
 |index|$\max(x)$|final $x$|$\bar{x}$
 | ---- | ---- | ---- | ---- |
@@ -42,12 +44,34 @@ Now for 1000 runs, I should store minimum position, final position, maximum posi
 Also, I updated the random choice number to be `np.random.choice([-1, 1])` which is the same but now it allows me to do `x+=step`.
 
 
-![bell-curve](./assets/1000runs.png)
+![bell-curve](./assets/1D/1000runs.png)
 
 Now I should plot a histogram of the final position, since theoretically it should show a bell curve, with a peak around 30-ish. Since $\sigma \propto \sqrt{N}$ therefore $\sigma \approx \sqrt{1000}$ therefore $\sigma \approx 31.62$.
 
 Now, running 4000:
 
-![bell-curve](./assets/2000runs.png)
+![bell-curve](./assets/1D/2000runs.png)
 
 Logs in `logs/4000runs.txt`. 
+
+# 2D motion
+To add 2D motion, I could do the following:
+- generate random number between 0 and 4
+- if 0, x += 1
+- if 1, x -= 1
+- if 2, y += 1
+- if 3, y -= 1
+
+(Later, I'd do them together)
+
+I can also calculate the distance by the pythagorean theorem.
+
+$$
+c = \sqrt{x^2 + y^2}
+$$
+
+Now I simulated this with 10,000, and the resulting data is in `logs/10000runs.txt`. The histograph and scatter plot of positions is as follows:
+
+![scatter-plot](./assets/2D/10000runs.png)
+
+![hist-plot](./assets/2D/10000runshist.png)
--- Random Walks/main.py
+++ Random Walks/main.py
@@ -1,44 +1,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = 0 # initial position
-ys = [] # positions
-xs = [] # time
+xs = []  # x-positions
+ys = []  # y-position
+times = []
+distance = []
 
 
-def walk(n_steps=1000):
-    x = 0
-    positions = []
+def walk(n_steps):
+    x = 0  # initial position-x
+    y = 0  # initial position-y
+    positions_x = []
+    positions_y = []
 
     for _ in range(n_steps):
-        step = np.random.choice([-1, 1])
-        x += step
-        positions.append(x)
+        direction = np.random.randint(4)
+        if direction == 0:
+            x += 1
+        elif direction == 1:
+            x -= 1
+        elif direction == 2:
+            y += 1
+        elif direction == 3:
+            y -= 1
+
+        positions_x.append(x)
+        positions_y.append(y)
 
     return {
-        "min": min(positions),
-        "final": positions[-1],
-        "max": max(positions),
-        "max_abs": np.max(np.abs(positions)),
+        "min_x": min(positions_x),
+        "min_y": min(positions_y),
+        "final_x": positions_x[-1],
+        "final_y": positions_y[-1],
+        "max_x": max(positions_x),
+        "max_y": max(positions_y),
+        "max_abs_x": np.max(np.abs(positions_x)),
+        "max_abs_y": np.max(np.abs(positions_y)),
     }
 
 
 with open("logs.txt", "w") as f:
-    f.write("| min | final | max | max abs |\n")
+    f.write(
+        "| min_x | final_x | max_x | max_abs_x | min_y | final_y | max_y | max_abs_y |\n"
+    )
 
-    for sim in range(4000):
+    for sim in range(10000):
+        result = walk(10000)
 
-        result = walk(4000)
-        ys.append(result['final'])
-        xs.append(sim)
         f.write(
-            f"| {result['min']} | "
-            f"{result['final']} | "
-            f"{result['max']} | "
-            f"{result['max_abs']} |\n"
+            f"| {result['min_x']} | "
+            f"{result['final_x']} | "
+            f"{result['max_x']} | "
+            f"{result['max_abs_x']} | "
+            f"{result['min_y']} | "
+            f"{result['final_y']} | "
+            f"{result['max_y']} | "
+            f"{result['max_abs_y']} |\n"
         )
 
-plt.hist(ys)
+        xs.append(result["final_x"])
+        ys.append(result["final_y"])
+        times.append(sim)
+        distance.append(np.sqrt(result["final_x"] ** 2 + result["final_y"] ** 2))
+
+# plt.scatter(xs, ys, s=1)
+plt.hist(distance)
+plt.axis("equal")
 plt.show()
-print(np.mean(ys))
-print(np.std(ys))
